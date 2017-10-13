bfe2cc988df7152d2beb1d52fd10bf5cbebc843e
Moncef Belyamani <moncef.belyamani@gsa.gov>
2017-10-12 20:29:27 -0400

    Add exception_notification gem
    **Why**: To allow us to receive real-time notifications via email
    any time an error occurs in production. The gem also has support for
    other notification channels, such as Slack. It also captures Sidekiq
    errors out of the box.
    
    Using this gem is a lot easier and cheaper than maintaining
    a self-hosted version of Sentry or Bugsnag, for example.
    
    Visit the gem's repo for more configuration options:
    https://github.com/smartinez87/exception_notification

---
+++ config/initializers/exception_notification.rb
@@ -0,0 +1,14 @@
+require 'exception_notification/rails'
+require 'exception_notification/sidekiq'
+
+EXCEPTION_RECIPIENTS = JSON.parse(Figaro.env.exception_recipients).freeze
+
+ExceptionNotification.configure do |config|
+  config.add_notifier(
+    :email,
+    email_prefix: "[#{APP_NAME} EXCEPTION] ",
+    sender_address: %("Exception Notifier" <notifier@login.gov>),
+    exception_recipients: EXCEPTION_RECIPIENTS,
+    error_grouping: true
+  )
+end
