7225dc577711d82d727cd3429b97b003862464df
Moncef Belyamani <moncef.belyamani@gsa.gov>
2017-10-17 00:34:27 -0400

    Add domain name to email prefix

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -6,7 +6,7 @@ EXCEPTION_RECIPIENTS = JSON.parse(Figaro.env.exception_recipients).freeze
 ExceptionNotification.configure do |config|
   config.add_notifier(
     :email,
-    email_prefix: "[#{APP_NAME} EXCEPTION] ",
+    email_prefix: "[#{APP_NAME} EXCEPTION - #{Figaro.env.domain_name}] ",
     sender_address: %("Exception Notifier" <notifier@login.gov>),
     exception_recipients: EXCEPTION_RECIPIENTS,
     error_grouping: true
